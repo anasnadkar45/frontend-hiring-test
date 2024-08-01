81594fc260fa651f0852d6d91f743a48195b37a4
anasnadkar45 <anasnadkar23@gmail.com>
2024-08-01 07:39:35 +0530

    Initial Setup for shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios';
+import React from 'react'
 
-function App() {
-  const [employee, setEmployee] = useState([]);
-
-  useEffect(() => {
-    FetchEmploye();
-    console.log(employee);
-  }, [])
-
-  const FetchEmploye = async () => {
-    const employees = await axios('https://free-ap-south-1.cosmocloud.io/development/api/employees',
-      {
-        headers: {
-          'projectId': '66aa2910440310e3620e0bbc',
-          'environmentId': '66aa2910440310e3620e0bbd',
-        },
-      }
-    );
-    setEmployee(employees.data);
-  }
+const App = () => {
   return (
-    <>
-
-    </>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
